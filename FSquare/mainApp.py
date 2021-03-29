798b27aba0aca086bbd53c182465fbd4ece2aaea
zoecchheenn@gmail.com <Znfgithub374>
2021-03-29 20:00:44 +1100

    chenchen: added backend for travel and isolation, added backend for all blog details, added read-more frontend and backend functions

--- FSquare/mainApp.py
+++ FSquare/mainApp.py
@@ -5,36 +5,14 @@ import pymysql
 from dbConnection import Database
 
 app = Flask(__name__)
-
+db = Database()
 # @app.route('/')
 # def home():
 #     return app.send_static_file('templates/Covax/about.html')
 
 def db_query(sql):
-    db = Database()
     rs = db.get_from_table(sql)
     return rs
-
-
-#about-us page
-@app.route('/about')
-def about():
-    return render_template('about1.html')
-
-#blog page
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-#blog detail page
-@app.route('/blogdetail')
-def blogdetail():
-    sql = "SELECT * FROM mp24.articles WHERE category = 'mask'"
-    res = db_query(sql)
-    res = res[1]
-    return render_template('blog-details.html',result=res, content_type='application/json')
-
-
 
 #home page
 @app.route('/')
@@ -44,6 +22,66 @@ def home():
 @app.route('/home')
 def home1():
     return render_template('index.html')
+
+
+
+#about-us page
+@app.route('/about')
+def about():
+    return render_template('about1.html')
+
+#blog page
+@app.route('/precaution')
+def blog():
+    return render_template('blog.html')
+
+#get detailed blog entry from database
+def get_detail(category,id):
+    sql = "SELECT * FROM mp24.articles WHERE category = '"+category+"' AND id = " + id
+    print(sql)
+    res = db_query(sql)
+    #get the only element in the res list
+    res = res[0]
+    return res
+
+
+
+#mask detail page
+@app.route('/maskdetail/<variable>', methods=['GET'])
+def maskdetail(variable):
+    res = get_detail('mask',variable)
+    return render_template('blog-details.html',result=res, content_type='application/json')
+
+#hygiene detail page
+@app.route('/hygienedetail/<variable>', methods=['GET'])
+def hygienedetail(variable):
+    res = get_detail('good hygiene',variable)
+    return render_template('blog-details.html',result=res, content_type='application/json')
+
+#travel detail page
+@app.route('/traveldetail/<variable>', methods=['GET'])
+def traveldetail(variable):
+    res = get_detail('travel restriction',variable)
+    print(res)
+    return render_template('blog-details.html',result=res, content_type='application/json')
+
+#isolation detail page
+@app.route('/isolationdetail/<variable>', methods=['GET'])
+def isolationdetail(variable):
+    res = get_detail('isolation',variable)
+    return render_template('blog-details.html',result=res, content_type='application/json')
+
+#transmission detail page
+@app.route('/transmissiondetail/<variable>', methods=['GET'])
+def transmissiondetail(variable):
+    res = get_detail('transmission',variable)
+    return render_template('blog-details.html',result=res, content_type='application/json')
+
+#symptom detail page
+@app.route('/symptomdetail/<variable>', methods=['GET'])
+def symptomdetail(variable):
+    res = get_detail('symptom',variable)
+    return render_template('blog-details.html',result=res, content_type='application/json')
 
 
 # #test db connection
@@ -74,11 +112,25 @@ def transmission():
             if key == "body":
                 dic[key] = dic[key][0:150]
     return render_template('transmission.html',result=res, content_type='application/json')
-#read more should be renamed since other page still got rename
+
+#return travel restriction page
+@app.route('/travel_restriction')
+def travel_restriction():
+    #return transmission.html
+    sql = "SELECT * FROM mp24.articles WHERE category = 'travel restriction'"
+    res = db_query(sql)
+    #shorten the body preview
+    #iterate the res list
+    for dic in res:
+    #iterate the dict in the res list
+        for key in dic:
+            if key == "body":
+                dic[key] = dic[key][0:150]
+    return render_template('travel.html',result=res, content_type='application/json')
 
 
 #return hygiene page
-@app.route('/hygiene')
+@app.route('/good_hygiene')
 def hygiene():
     #return hygiene.html
     sql = "SELECT * FROM mp24.articles WHERE category = 'good hygiene'"
@@ -107,6 +159,21 @@ def symptom():
                 dic[key] = dic[key][0:150]
     return render_template('symptom.html',result=res, content_type='application/json')
 
+#return isolation page
+@app.route('/isolation')
+def isolation():
+    #return hygiene.html
+    sql = "SELECT * FROM mp24.articles WHERE category = 'isolation'"
+    res = db_query(sql)
+    #shorten the body preview
+    #iterate the res list
+    for dic in res:
+    #iterate the dict in the res list
+        for key in dic:
+            if key == "body":
+                dic[key] = dic[key][0:150]
+    return render_template('isolation.html',result=res, content_type='application/json')
+
 
 #returns mask page
 @app.route('/mask')
@@ -122,11 +189,6 @@ def mask():
                 dic[key] = dic[key][0:150]
     return render_template('mask.html',result=res, content_type='application/json')
 
-#returns "read more" result for blog pages
-@app.route('/mask_articles/<articlenum>')
-def read_more(articlenum):
-    results1 = request.args.get('results1', '0-100')
-    return articlenum
 
 
 # #vaccine page
@@ -136,6 +198,6 @@ def read_more(articlenum):
 #     return render_template('vaccine.html',result=res, content_type='application/json')
 
 #test page
-@app.route('/test')
-def test():
-    return render_template('test.html', content_type='application/json')
+# @app.route('/test')
+# def test():
+#     return render_template('test.html', content_type='application/json')
